193dbc1992e74d3d3ce9b0c8ee7eb0548d01a688
beingzuhairkhan <zuhairkhan5134@gmail.com>
2024-07-12 22:51:49 +0530

    Update Toppers.tsx

--- app/components2/Toppers.tsx
+++ app/components2/Toppers.tsx
@@ -8,18 +8,22 @@ type Props = {};
 
 const Toppers = (props: Props) => {
   return (
-    <div>
-       <h1 className="text-3xl font-bold mb-4 flex justify-center dark:text-white text-black"><span className="text-gradient">Garuda&apos;s </span> Toppers</h1>
-      <div className="w-full flex flex-wrap justify-center gap-4 p-1">
+    <div className="max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 flex justify-center dark:text-white text-black">
+        <span className="text-gradient">Garuda&apos;s </span> Toppers
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {[T1, T2, T3].map((image, index) => (
           <div
             key={index}
-            className="group relative w-full sm:w-96 h-64 md:w-120 md:h-72 lg:w-174 lg:h-80 overflow-hidden rounded-lg shadow-lg transform transition-all duration-500 hover:scale-105 hover:shadow-2xl"
+            className="group relative overflow-hidden rounded-lg shadow-lg transform transition-all duration-500 hover:scale-105 hover:shadow-2xl"
           >
             <Image
               src={image}
               alt={`Topper ${index + 1}`}
-              layout="fill"
+              layout="responsive"
+              width={280}
+              height={200}
               objectFit="cover"
               className="transform transition-transform duration-500 group-hover:scale-110"
             />
@@ -30,9 +34,10 @@ const Toppers = (props: Props) => {
           </div>
         ))}
       </div>
-      <hr className="mt-2 mb-2" />
+      <hr className="mt-4" />
     </div>
   );
 };
 
 export default Toppers;
+
